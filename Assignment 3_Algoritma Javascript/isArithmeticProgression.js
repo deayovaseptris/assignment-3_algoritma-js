d4139295c73905dbd3717a16ba7d504ff367c80e
Dea Yova Septri Saragih <yovaseptri3@gmail.com>
2024-10-10 20:44:14 +0700

    Assignment 3_Algoritma Javascript

---
+++ Assignment 3_Algoritma Javascript/isArithmeticProgression.js
@@ -0,0 +1,22 @@
+function isArithmeticProgression(numbers) {
+  if (numbers.length < 2) {
+    return true;
+  }
+
+  const diff = numbers[1] - numbers[0];
+
+  for (let i = 2; i < numbers.length; i++) {
+    if (numbers[i] - numbers[i - 1] !== diff) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test Cases
+console.log(isArithmeticProgression([1, 2, 3, 4, 5, 6])); //true
+console.log(isArithmeticProgression([2, 4, 6, 12, 24])); //false
+console.log(isArithmeticProgression([2, 4, 6, 8])); //true
+console.log(isArithmeticProgression([2, 6, 18, 54])); //false
+console.log(isArithmeticProgression([1, 2, 3, 4, 7, 9])); //false
